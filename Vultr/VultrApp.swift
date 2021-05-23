dd3c13b0bff2d461c8cf8430e52d5660c063d2b5
Henry Paulino <henrypl360@gmail.com>
2021-05-23 12:14:34 -0400

    Initial Commit

---
+++ Vultr/VultrApp.swift
@@ -0,0 +1,17 @@
+//
+//  VultrApp.swift
+//  Vultr
+//
+//  Created by Henry Paulino on 5/23/21.
+//
+
+import SwiftUI
+
+@main
+struct VultrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
